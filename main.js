4ae2a62cb97e8a656423239604539b92d22bc18b
bruno34154 <www.brunomesenga@gmail.com>
2022-03-02 10:02:38 -0300

    refactored functions color, fontfam and align

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ var textoclone;
 var clonesalt = [];
 
 
-
 /*tamanho texto*/
 function size(){
     try{
@@ -35,73 +34,58 @@ function size(){
 
 fsize.addEventListener("keydown", size);
 
+//alinhar texto
 
-/*alinhar texto*/
-function left(){
-	texto.style.textAlign = "left";
-	   for(i=0; i<clonesalt.length; i++){
-		clonesalt[i].style.textAlign = "left";
-    }
-
-}
-function right(){
-	texto.style.textAlign = "right";
+function TextAlign (Alignment){
+	texto.style.textAlign = Alignment;
 	  for(i=0; i<clonesalt.length; i++){
-		clonesalt[i].style.textAlign = "right";
-    }
-}
-function center(){
-	texto.style.textAlign = "center";
-	  for(i=0; i<clonesalt.length; i++){
-		clonesalt[i].style.textAlign = "center";
-    }
-}
-function justify(){
-	texto.style.textAlign = "justify";
-	  for(i=0; i<clonesalt.length; i++){
-		clonesalt[i].style.textAlign = "justify";
+		clonesalt[i].style.textAlign = Alignment;
     }
 }
 
 /*tipo de fonte*/
 function fontFam(){ 
 
-	if(select_font.value == "helvetica"){
-		texto.style.fontFamily = "helvetica";
-		for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.fontFamily = "helvetica";
-    	}
-	}
-	else if(select_font.value == "arial"){
-		texto.style.fontFamily = "arial";
-		for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.fontFamily = "arial";
-    	}
-
-	}
-	else if(select_font.value == "impact"){
-		texto.style.fontFamily = "impact";
-		for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.fontFamily = "impact";
-    	}
-	}
-	else if(select_font.value == "charcoal"){
-		texto.style.fontFamily = "charcoal";
-		for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.fontFamily = "charcoal";
-    	}
-	}
-	else if(select_font.value == "sansserif"){
-		texto.style.fontFamily = "sansSerif";
-		for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.fontFamily = "sansSerif";
-    	}
-	}
-	else if(select_font.value == "times"){
-		texto.style.fontFamily = "times";
-		for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.fontFamily = "times";
-    	}
+	switch(select_font.value){
+		case "helvetica": 
+			texto.style.fontFamily = "helvetica";
+			for(i=0; i<clonesalt.length; i++){
+	      		clonesalt[i].style.fontFamily = "helvetica";
+    		}
+    		break;
+    	case "arial":
+    		texto.style.fontFamily = "arial";
+			for(i=0; i<clonesalt.length; i++){
+	      		clonesalt[i].style.fontFamily = "arial";
+    		}
+    		break;
+    	case "impact":
+    		texto.style.fontFamily = "impact";
+			for(i=0; i<clonesalt.length; i++){
+	      		clonesalt[i].style.fontFamily = "impact";
+    		}
+    		break;
+    	case "charcoal":
+    		texto.style.fontFamily = "charcoal";
+			for(i=0; i<clonesalt.length; i++){
+	      		clonesalt[i].style.fontFamily = "charcoal";
+    		}
+    		break;
+    	case "sansserif":
+    		texto.style.fontFamily = "sansSerif";
+			for(i=0; i<clonesalt.length; i++){
+	      		clonesalt[i].style.fontFamily = "sansSerif";
+    		}
+    		break;
+    	case times:
+    		texto.style.fontFamily = "times";
+			for(i=0; i<clonesalt.length; i++){
+	      		clonesalt[i].style.fontFamily = "times";
+    		}
+    		break;
+    	default:
+    		//faz nada
+    		break;
 	}
 
 }
@@ -114,6 +98,8 @@ checknegrito.addEventListener("change", ()=>{
         for(i=0; i<clonesalt.length; i++){
 	      clonesalt[i].style.fontWeight = "bold";
     	} 
+
+    	
 	}else{
 		texto.style.fontWeight = "normal";
         for(i=0; i<clonesalt.length; i++){
@@ -152,157 +138,17 @@ checkitalico.addEventListener("change", ()=>{
     }
     });
 
-
-  
-
-  /*cores*/
-
- function preto(){
- 	texto.style.color = "black";
- 	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "black";
-    	}
- 	
-  }
-  function cinzaescuro(){
-  	texto.style.color = "#545454"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#545454";
-    	}
-  }
-  function cinzaclaro(){
-  	texto.style.color = "#d9d9d9"
-  		for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#d9d9d9";
-    	}
-  }
-  function branco(){
-  	texto.style.color = "white"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "white";
-    	}
-  }
-  function vermelho(){
-
-  	texto.style.color = "#ff1616"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#ff1616";
-    	}
-  }
  
-  function vermelhocoral(){
 
-  	texto.style.color = "#ff5757"
+//Mudar cor
+
+  function setColor(color) {
+
+  	texto.style.color = color;
   	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#ff5757";
+	      clonesalt[i].style.color = color;
     	}
 
-  }
-  function rosa(){
-  	texto.style.color = "#ff66c4"
-  		for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#ff66c4";
-    	}
-
-  }
-  function margenta(){
-
-  	texto.style.color = "#cb6ce6"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#cb6ce6";
-    	}
-  }
-  function roxo(){
-
-  	texto.style.color = "#8c52ff"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#8c52ff";
-    	}
-  }
-  function violeta(){
-  	texto.style.color = "#5e17eb"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#5e17eb";
-    	}
-  }
-  function turquesa(){
-
-  	texto.style.color = "#03989e"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#03989e";
-    	}
-
-  }
-  function azulagua(){
-
-  	texto.style.color = "#00c2cb"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#00c2cb";
-    	}
-
-  }
-  function azulturquesa(){
-
-  	texto.style.color = "#5ce1e6"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#5ce1e6";
-    	}
-
-  }
-  function azulclaro(){
-
-  	texto.style.color = "#38b6ff"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#38b6ff";
-    	}
-
-  }
-
-  function azul(){
-  	texto.style.color = "#5271ff"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#5271ff";
-    	}
-  }
-
-   function cobalto(){
-  	texto.style.color = "#004aad"
-  		for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#004aad";
-    	}
-
-  }
-   function verde(){
-  	texto.style.color = "#008037"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#008037";
-    	}
-  }
-   function verdefolha(){
-  	texto.style.color = "#7ed957"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#7ed957";
-    	}
-  }
-  
-   function verdelimao(){
-  	texto.style.color = "#69e265"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#69e265";
-    	}
-
-  }
-   function amarelo(){
-  	texto.style.color = "#ffde59"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#ffde59";
-    	}
-  }
-   function laranja(){
-  	texto.style.color = "#ff914d"
-  	for(i=0; i<clonesalt.length; i++){
-	      clonesalt[i].style.color = "#ff914d";
-    	}
   }
 
   /*contorno da fonte*/
